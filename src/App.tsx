1bc14f2ffda0f77dbe7fb1c317cf7d79f9352c5a
Lelka00777 <>
2022-07-23 12:50:12 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+import { Lamp } from "./Components/Lamp/Lamp";
+import { Hart } from "./Components/Hart/Hart";
+
+export default function App() {
+  return <div className="app">
+
+
+
+
+  </div>
+}
---
+++ src/Components/Ball/Ball.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import s from './Ball.module.css';
+import {findAllInRenderedTree} from "react-dom/test-utils";
+export const Ball = () => {
+
+
+
+    return (
+        <div className={s.bar}>
+
+            <div className={s.ball}>
+
+            </div>
+
+
+        </div>
+    );
+};
+
